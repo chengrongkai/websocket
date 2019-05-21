88d04bef75f4a55ce643813b565557b2e1d539a0
程荣凯 <1058831654@qq.com>
2019-05-21 22:40:43 +0800

    Initial commit

---
+++ src/main/java/com/crk/websocket/entity/WebSocketMessage.java
@@ -0,0 +1,17 @@
+package com.crk.websocket.entity;
+
+/**
+ * @Author: 程荣凯
+ * @Date: 2018/5/25 11:11
+ */
+public class WebSocketMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
